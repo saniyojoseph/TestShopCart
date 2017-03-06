94463fddaaee707c48493d78eeb44614d16b7945
Sanio <saniojoseph@gmail.com>
2017-03-06 06:26:59 -0800

    first commit

---
+++ js/controller.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+// store: contains the product list
+// cart: the shopping cart object
+function storeController($scope, $routeParams, DataService) {
+
+    // get store and cart
+    $scope.store = DataService.store;
+    $scope.cart = DataService.cart;
+
+    // routing to pick the selected product
+    if ($routeParams.productSku != null) {
+        $scope.product = $scope.store.getProduct($routeParams.productSku);
+    }
+}
---
+++ js/store.js
@@ -0,0 +1,39 @@
+﻿//----------------------------------------------------------------
+// store (contains the products)
+//
+function store() {
+    this.products = [
+        new product("APL", "HTC", "Model:Desire ", 12, 9.0, 0, 2, 0, 1, 2),
+        new product("AVC", "Samsung", "Model:A5", 16, 6, 0, 1, 1, 1, 2),
+        new product("BAN", "Motorola","Model:Moto Max", 4, 5, 0, 2, 1, 2, 2),
+        new product("FIG", "HTC", "Model:Desire plus", 10, 4, 0, 0, 0, 1, 2),
+        new product("GRF", "LG", "Model:Sharp", 11, 5, 4, 4, 1, 1, 1),
+        new product("GRP", "Lenovo", "Model:Phab Plus", 8, 8, 0, 3, 0, 1, 1),
+        new product("PAP", "LG", "Model:G4", 5, 3, 3, 4, 2, 2, 2),
+        new product("PCH", "Motorola", "Model:M2", 19, 7, 1, 2, 0, 1, 2),
+        new product("STR", "Samsung", "Model:J7", 7, 4, 0, 4, 1, 1, 2),
+        new product("TNG", "Oppo", "Model:Selfie Cam", 8, 5, 3, 4, 1, 1, 2),
+        new product("WML", "Lenovo", "Model:K4 note", 4, 9, 4, 4, 0, 1, 1)
+    ];
+    this.dvaCaption = [
+        "Negligible",
+        "Low",
+        "Average",
+        "Good",
+        "Great"
+    ];
+    this.dvaRange = [
+        "below 5%",
+        "between 5 and 10%",
+        "between 10 and 20%",
+        "between 20 and 40%",
+        "above 40%"
+    ];
+}
+store.prototype.getProduct = function (sku) {
+    for (var i = 0; i < this.products.length; i++) {
+        if (this.products[i].sku == sku)
+            return this.products[i];
+    }
+    return null;
+}
